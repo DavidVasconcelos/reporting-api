362978084502ad20db93d3c13bf1c9aaea12d731
David Vasconcelos <dsv10@hotmail.com>
2022-04-14 16:49:42 -0300

    securiting refactor

--- src/main/kotlin/com/speedyteller/reporting/api/security/JwtTokenFilter.kt
+++ src/main/kotlin/com/speedyteller/reporting/api/security/JwtTokenFilter.kt
@@ -1,7 +1,6 @@
 package com.speedyteller.reporting.api.security
 
 import com.speedyteller.reporting.api.config.JwtTokenComponent
-import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.http.HttpHeaders
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken
 import org.springframework.security.core.context.SecurityContextHolder
@@ -9,7 +8,6 @@ import org.springframework.security.core.userdetails.UserDetails
 import org.springframework.security.core.userdetails.UserDetailsService
 import org.springframework.security.web.authentication.WebAuthenticationDetailsSource
 import org.springframework.stereotype.Component
-import org.springframework.util.StringUtils
 import org.springframework.web.filter.OncePerRequestFilter
 import javax.servlet.FilterChain
 import javax.servlet.http.HttpServletRequest
@@ -26,9 +24,8 @@ class JwtTokenFilter(
         response: HttpServletResponse,
         filterChain: FilterChain
     ) {
-
         val header = request.getHeader(HttpHeaders.AUTHORIZATION)
-        if (StringUtils.hasLength(header)) {
+        if (header.isNullOrEmpty()) {
             filterChain.doFilter(request, response)
             return
         }
