e87f93e4cedf59a8f32ad75e27bc155d24c82a8c
David Vasconcelos <dsv10@hotmail.com>
2022-04-17 09:39:08 -0300

    fix docker environment

--- src/main/resources/db/migration/V1__.sql
+++ src/main/resources/db/migration/V1__.sql
@@ -4,15 +4,3 @@ CREATE TABLE speedyteller.sample_entity (
     description varchar(255),
     CONSTRAINT sample_entity_pkey PRIMARY KEY (id)
 );
-CREATE TABLE speedyteller.sample_entity2 (
-    id uuid NOT NULL,
-    title varchar(30),
-    description varchar(255),
-    CONSTRAINT sample_entity_pkey2 PRIMARY KEY (id)
-);
-CREATE TABLE speedyteller.sample_entity3 (
-    id BIGSERIAL NOT NULL,
-    title varchar(30),
-    description varchar(255),
-    CONSTRAINT sample_entity_pkey3 PRIMARY KEY (id)
-);
