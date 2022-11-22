6fbc46be6cb09eceeb9d240811b9923589ce4ed2
David Vasconcelos <dsv10@hotmail.com>
2022-11-22 16:21:29 -0300

    fix query again

--- src/main/kotlin/com/speedyteller/reporting/api/domain/constant/BusinessConstants.kt
+++ src/main/kotlin/com/speedyteller/reporting/api/domain/constant/BusinessConstants.kt
@@ -62,7 +62,7 @@ object BusinessConstants {
                        a.code as acquirerCode,
                        a.type as acquirerType,
                        tr.refundable
-                FROM speedyteller.transaction tr left join fx_transaction ft on tr.fx_transaction_td = ft.id
+                FROM speedyteller.transaction tr left join speedyteller.fx_transaction ft on tr.fx_transaction_td = ft.id
                 left join speedyteller.customer c on tr.customer_id = c.id
                 left join speedyteller.merchant m on tr.merchant_id = m.id
                 left join speedyteller.instant_payment_notification ipn on tr.transaction_id = ipn.transaction_id
