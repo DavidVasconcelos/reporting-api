bf8efd50ddb0c957612722b3de8d894722addb6a
David Vasconcelos <dsv10@hotmail.com>
2022-12-14 11:05:39 -0300

    caching refactoring

--- src/main/kotlin/com/speedyteller/reporting/api/cache/SpringCacheCustomizerComponent.kt
+++ src/main/kotlin/com/speedyteller/reporting/api/cache/SpringCacheCustomizerComponent.kt
@@ -1,12 +1,16 @@
 package com.speedyteller.reporting.api.cache
 
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.boot.autoconfigure.cache.CacheManagerCustomizer
 import org.springframework.cache.concurrent.ConcurrentMapCacheManager
 import org.springframework.stereotype.Component
 
 @Component
 class SpringCacheCustomizerComponent : CacheManagerCustomizer<ConcurrentMapCacheManager> {
+
+    @Value("#{'\${caching.spring.list}'.split(';')}")
+    private lateinit var cacheList: List<String>
     override fun customize(cacheManager: ConcurrentMapCacheManager) {
-        cacheManager.setCacheNames(listOf("logins", "users", "transactions", "customers"))
+        cacheManager.setCacheNames(cacheList)
     }
 }
--- src/main/kotlin/com/speedyteller/reporting/api/cache/SpringCachingConfig.kt
+++ src/main/kotlin/com/speedyteller/reporting/api/cache/SpringCachingConfig.kt
@@ -1,5 +1,6 @@
 package com.speedyteller.reporting.api.cache
 
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.cache.CacheManager
 import org.springframework.cache.concurrent.ConcurrentMapCacheManager
 import org.springframework.context.annotation.Bean
@@ -8,8 +9,12 @@ import org.springframework.context.annotation.Configuration
 @Configuration
 class SpringCachingConfig {
 
+    @Value("#{'\${caching.spring.list}'.split(';')}")
+    private lateinit var cacheList: List<String>
+
     @Bean
+    @Suppress("SpreadOperator")
     fun cacheManager(): CacheManager {
-        return ConcurrentMapCacheManager("logins", "users", "transactions", "customers")
+        return ConcurrentMapCacheManager(*cacheList.toTypedArray())
     }
 }
