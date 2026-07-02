079fd7e3de8eec0e224f8bb24869b7d349ab2f88
David Vasconcelos <dsv10@hotmail.com>
2026-07-02 11:40:07 -0300

    fixing security

--- src/main/kotlin/com/speedyteller/reporting/api/security/JwtTokenFilter.kt
+++ src/main/kotlin/com/speedyteller/reporting/api/security/JwtTokenFilter.kt
@@ -1,6 +1,5 @@
 package com.speedyteller.reporting.api.security
 
-import com.speedyteller.reporting.api.common.JwtTokenComponent
 import jakarta.servlet.FilterChain
 import jakarta.servlet.http.HttpServletRequest
 import jakarta.servlet.http.HttpServletResponse
