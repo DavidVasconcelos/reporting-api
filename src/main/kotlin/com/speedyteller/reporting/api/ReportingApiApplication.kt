9348b63669c20d05c1b1d0a0649a4dad2e853074
David Vasconcelos <dsv10@hotmail.com>
2026-07-03 09:50:11 -0300

    upgrade redis

--- src/main/kotlin/com/speedyteller/reporting/api/ReportingApiApplication.kt
+++ src/main/kotlin/com/speedyteller/reporting/api/ReportingApiApplication.kt
@@ -3,10 +3,8 @@ package com.speedyteller.reporting.api
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.cache.annotation.EnableCaching
-import org.springframework.scheduling.annotation.EnableScheduling
 
 @EnableCaching
-@EnableScheduling
 @SpringBootApplication
 class ReportingApiApplication
 
